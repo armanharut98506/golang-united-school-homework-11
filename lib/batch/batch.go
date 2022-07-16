f1dcaf124c31460b737870cca635999c2fe6c798
armanharut98506 <armanharut7117@gmail.com>
2022-07-17 01:41:53 +0400

    Solution for task11

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -13,6 +13,29 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
+func worker(id int64, jobs <-chan int64, results chan<- user) {
+	for j := range jobs {
+		results <- getOne(j)
+	}
+}
+
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	jobs := make(chan int64, n)
+	results := make(chan user, n)
+
+	for i := int64(0); i < pool; i++ {
+		go worker(i, jobs, results)
+	}
+
+	for j := int64(0); j < n; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	users := make([]user, 0, n)
+	for i := int64(0); i < n; i++ {
+		users = append(users, <-results)
+	}
+	
+	return users
 }
